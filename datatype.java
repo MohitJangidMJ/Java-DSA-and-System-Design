8320d355682a67276ee35759f500cca9109d9a95
MohitJangidMJ <mohitjangid6350@gmail.com>
2023-02-06 16:00:26 +0530

    new program

--- datatype.java
+++ datatype.java
@@ -2,7 +2,8 @@ public class datatype {
     
          public static void main(String[] args) 
         {
-            byte data=45;
+           // byte data=45;
+           byte data=129;
             System.out.println(data);
         }
     
---
+++ type casting.java
@@ -0,0 +1,8 @@
+public public static void main(String[] args) {
+    {
+    int a=45;
+    double b;
+   b=a;
+   System.out.println(b);
+}
+}
